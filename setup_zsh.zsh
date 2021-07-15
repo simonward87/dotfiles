74ee9c9521a4bacca197938adba8ebceb0d0a284
Simon Ward <39803787+simonward87@users.noreply.github.com>
2021-07-15 13:36:02 +0100

    Prevent superfluous commands with conditionals
    Symlink sh to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,8 +4,24 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation unnecessary; it's in the Brewfile.
 
-echo 'Enter superuser (sudo) password to edit /etc/shells'
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+if grep -Fxq /usr/local/bin/zsh /etc/shells; then
+  echo '/usr/local/bin/zsh already exists in /etc/shells';
+else
+  echo 'Enter superuser (sudo) password to edit /etc/shells';
+  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null;
+fi
 
-echo 'Enter user password to change login shell'
-chsh -s '/usr/local/bin/zsh'
+if grep -q '/usr/local/bin/zsh' <<< "$SHELL"; then
+  echo '$SHELL is already /usr/local/bin/zsh';
+else
+  echo 'Enter user password to change login shell'
+  chsh -s '/usr/local/bin/zsh'
+fi
+
+if sh --version | grep -q zsh; then
+  echo '/private/var/select/sh already linked to /bin/zsh'
+else
+  echo "Enter superuser (sudo) password to symlink sh to zsh"
+  # Looked cute, might delete later, idk
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+fi
