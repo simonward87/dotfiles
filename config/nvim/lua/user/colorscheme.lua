26ec49efef8d30de0c454e0405bc6a21b8d1ae18
Simon Ward <39803787+simonward87@users.noreply.github.com>
2022-01-04 10:33:59 +0000

    Updated colorscheme

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,10 @@
+vim.o.background = "light"
+
 -- local colorscheme = "everforest"
 -- local colorscheme = "gruvbox-flat"
-local colorscheme = "tender"
+-- local colorscheme = "kanagawa"
+local colorscheme = "iceberg"
+-- local colorscheme = "tender"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
