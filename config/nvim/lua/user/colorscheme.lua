e42674502acbb1ccef696a74e5ce14d063e78446
Simon Ward <39803787+simonward87@users.noreply.github.com>
2022-02-28 11:26:27 +0000

    created branch for dark theme

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,5 +1,5 @@
-vim.o.background = "light"
-local colorscheme = "github"
+-- vim.o.background = "light"
+local colorscheme = "tender"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
