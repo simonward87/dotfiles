8b07d0fac445535da7fb4894580d6867a5083a91
Simon Ward <39803787+simonward87@users.noreply.github.com>
2022-08-15 15:25:21 +0100

    Update Neovim options to use full property names

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -7,19 +7,18 @@ vim.opt.hlsearch = false -- highlight doesn't persist after search
 vim.opt.ignorecase = true -- ignore case in search patterns
 vim.opt.iskeyword:append("-") -- treat split words as single words
 vim.opt.laststatus = 1 -- resets custom statusline colours
-vim.opt.ru = false -- hide ruler
-vim.opt.nu = true -- line numbers
-vim.opt.rnu = true -- relative line numbers
-vim.opt.ru = false -- hide character and line number ruler
+vim.opt.number = true -- line numbers
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.ruler = false -- hide character and line number ruler
 vim.opt.scrolloff = 4 -- vertical scroll boundaries
 vim.opt.sidescrolloff = 12 -- horizontal scroll boundaries
 vim.opt.signcolumn = "auto" -- combine signcolumn into number column
 vim.opt.smartcase = true -- smart case-insensitive search
-vim.opt.sts = 4 -- number of spaces used in place of a tab when expandtab is enabled
-vim.opt.sw = 4 -- number of spaces to use for autoindent
-vim.opt.swf = false -- stop autocreating swapfiles
+vim.opt.softtabstop = 4 -- number of spaces used in place of a tab when expandtab is enabled
+vim.opt.shiftwidth = 4 -- number of spaces to use for autoindent
+vim.opt.swapfile = false -- stop autocreating swapfiles
 vim.opt.termguicolors = true
-vim.opt.ts = 4 -- the width of a hardtab measured in spaces
+vim.opt.tabstop = 4 -- the width of a hardtab measured in spaces
 vim.opt.undofile = true -- create undo files
 vim.opt.wrap = false -- no line-wrapping
 
