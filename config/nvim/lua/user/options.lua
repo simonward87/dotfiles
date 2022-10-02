a610f08405d6e06320771942ac9a59022b68a16e
Simon Ward <39803787+simonward87@users.noreply.github.com>
2022-10-02 21:05:13 +0100

    Update nvim options

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -4,7 +4,6 @@ vim.opt.completeopt = { "menuone", "longest", "noinsert", "noselect" } -- text c
 vim.opt.cursorline = true -- highlight current line
 vim.opt.expandtab = true -- insert spaces for tabs
 vim.opt.fillchars = "eob: " -- hide end of buffer characters
-vim.opt.guicursor = "" -- use block cursor
 vim.opt.ignorecase = true -- ignore case in search patterns
 vim.opt.iskeyword:append("-") -- treat split words as single words
 vim.opt.laststatus = 0 -- resets custom status-line colours
@@ -16,6 +15,7 @@ vim.opt.sidescrolloff = 12 -- horizontal scroll boundaries
 vim.opt.smartcase = true -- smart case-insensitive search
 vim.opt.softtabstop = 4 -- number of spaces used in place of a tab when 'expandtab' is enabled
 vim.opt.shiftwidth = 4 -- number of spaces to use for auto-indent
+vim.opt.showmode = false -- hide mode indicators
 vim.opt.swapfile = false -- stop auto-creating swap-files
 vim.opt.termguicolors = true
 vim.opt.tabstop = 4 -- the width of a hard-tab measured in spaces
@@ -23,5 +23,6 @@ vim.opt.undofile = true -- create undo files
 vim.opt.wrap = false -- no line-wrapping
 
 -- vim.opt.cmdheight = 0 -- hides command line when not active
+-- vim.opt.guicursor = "" -- use block cursor
 -- vim.opt.signcolumn = "number" -- combine signcolumn into number column
 -- vim.opt.spell = true -- spellcheck comments
