f8e532ea7757f7404646dc0065126e192fa34759
Simon Ward <39803787+simonward87@users.noreply.github.com>
2022-10-12 16:58:54 +0100

    Reformat nvim plugins file

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -40,28 +40,36 @@ packer.init({
 
 -- plugin installs
 return packer.startup(function(use)
-	-- general utilities
 	use("akinsho/bufferline.nvim") -- bufferline along top of window
 	use("antoinemadec/FixCursorHold.nvim") -- fixes lsp doc highlight
+	use("JoosepAlviste/nvim-ts-context-commentstring") -- simple code commenting
 	use({
 		"kyazdani42/nvim-tree.lua", -- file explorer
 		requires = {
 			"kyazdani42/nvim-web-devicons", -- icons
 		},
 	})
+	use("lewis6991/gitsigns.nvim") -- git info in sign column
 	use("lewis6991/impatient.nvim") -- speeds up loading lua modules
 	use("lukas-reineke/indent-blankline.nvim") -- add guide rules to indentation
+	use("mfussenegger/nvim-dap") -- debug adapter
 	use("moll/vim-bbye") -- improve buffer deletion behaviour
 	use("norcalli/nvim-colorizer.lua") -- hex code colourizer
-	use("nvim-lua/popup.nvim") -- An implementation of vim Popup API
 	use("nvim-lua/plenary.nvim") -- lua function library (dependency for other plugs)
-	-- use("petertriho/nvim-scrollbar") -- diagnostics scrollbar
+	use("nvim-lua/popup.nvim") -- An implementation of vim Popup API
+	use("nvim-telescope/telescope.nvim") -- fuzzy finder
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("nvim-treesitter/nvim-treesitter-context") -- pin code context (current block)
 	use("tpope/vim-capslock") -- software capslock
 	use("tpope/vim-commentary") -- comment stuff out
 	use("tpope/vim-fugitive") -- git wrapper
 	use("tpope/vim-surround") -- streamline surroundings workflow
 	use("wbthomason/packer.nvim") -- Let packer manage itself
-	use("nvim-treesitter/nvim-treesitter-context") -- pin code context (current block)
+	use("windwp/nvim-autopairs") -- autopairs with cmp and treesitter integration
+	use("windwp/nvim-ts-autotag") -- auto-close & auto-rename html tags
 
 	-- colorschemes
 	use({ "catppuccin/nvim", as = "catppuccin" })
@@ -77,43 +85,23 @@ return packer.startup(function(use)
 	use("sainnhe/gruvbox-material")
 
 	-- completion
-	use("hrsh7th/nvim-cmp") -- completion plugin
 	use("hrsh7th/cmp-buffer") -- buffer completions
-	use("hrsh7th/cmp-path") -- path completions
 	use("hrsh7th/cmp-cmdline") -- cmdline completions
-	use("saadparwaiz1/cmp_luasnip") -- snippet completions
 	use("hrsh7th/cmp-nvim-lsp") -- lsp completions
 	use("hrsh7th/cmp-nvim-lua") -- complete neovim lua
-
-	-- snippets
+	use("hrsh7th/cmp-path") -- path completions
+	use("hrsh7th/nvim-cmp") -- completion plugin
 	use("L3MON4D3/LuaSnip") -- snippet engine
 	use("rafamadriz/friendly-snippets") -- snippet library
+	use("saadparwaiz1/cmp_luasnip") -- snippet completions
 
 	-- LSP
-	use("neovim/nvim-lspconfig") -- enable LSP
-	use("williamboman/nvim-lsp-installer") -- language server installer
-	use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
-	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
 	use("b0o/SchemaStore.nvim") -- schemastore catalog access
 	use("j-hui/fidget.nvim") -- UI for nvim-lsp install progress
-
-	-- telescope
-	use("nvim-telescope/telescope.nvim")
-
-	-- treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
-	use("JoosepAlviste/nvim-ts-context-commentstring") -- simple code commenting
-	use("windwp/nvim-autopairs") -- autopairs with cmp and treesitter integration
-	use("windwp/nvim-ts-autotag") -- auto-close & auto-rename html tags
-
-	-- gitsigns
-	use("lewis6991/gitsigns.nvim")
-
-	-- dap
-	use("mfussenegger/nvim-dap")
+	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+	use("neovim/nvim-lspconfig") -- enable LSP
+	use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
+	use("williamboman/nvim-lsp-installer") -- language server installer
 
 	-- automatically set up configuration after cloning packer
 	-- NOTE: must run after all plugins
