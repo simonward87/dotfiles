e6334ffc28d23086c7c0c55dd7e769a383687a1d
Simon Ward <39803787+simonward87@users.noreply.github.com>
2022-10-18 16:21:06 +0100

    Refactor tender.lua

--- config/nvim/lua/user/tender.lua
+++ config/nvim/lua/user/tender.lua
@@ -5,16 +5,17 @@ local hl = function(group, opts)
 	vim.api.nvim_set_hl(0, group, opts)
 end
 
+local bg = "#1d1d1d"
+
 hl("LineNr", {
 	fg = "#666666", -- increase contrast, alt: #476572
 })
 
 hl("Search", {
-	fg = "#1d1d1d",
+	fg = bg,
 	bg = "#c9d05c",
 })
 
-local bg = "#1d1d1d"
 local hl_groups = { "Normal", "SignColumn", "TreesitterContext" }
 
 for i = 1, #hl_groups do
