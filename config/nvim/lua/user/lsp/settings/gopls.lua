ad9db57973a22795f1f80b0945a25fb123d5434f
Simon Ward <39803787+simonward87@users.noreply.github.com>
2023-04-17 17:54:59 +0100

    Add gopls lsp custom settings
    Temporary work-around for an issue when using Go build tags that doesn't allow lsp to run correctly

---
+++ config/nvim/lua/user/lsp/settings/gopls.lua
@@ -0,0 +1,13 @@
+return {
+	settings = {
+		gopls = {
+			env = {
+				-- work-around for ls not running when using build tags:
+				-- https://github.com/golang/go/issues/29202
+				-- more info about GOFLAGS:
+				-- https://pkg.go.dev/cmd/go#hdr-Environment_variables
+				GOFLAGS = "-tags=integration,linux,test,unittest,windows",
+			},
+		},
+	},
+}
