9b2afaa369f706a279f59f6f5b85abb8d2238afd
Simon Ward <39803787+simonward87@users.noreply.github.com>
2023-10-02 11:40:45 +0100

    Update indent_blankline config to match new API

--- config/nvim/lua/user/indent.lua
+++ config/nvim/lua/user/indent.lua
@@ -1,11 +1,19 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
 	return
 end
 
 vim.opt.list = true
 
-indent_blankline.setup({
-	show_current_context = true,
-	show_current_context_start = false,
+ibl.setup({
+	indent = {
+		char = "▏",
+		tab_char = "▏",
+		-- char = "▎",
+		-- tab_char = "▎",
+	},
+	scope = {
+		show_start = false,
+		show_end = false,
+	},
 })
