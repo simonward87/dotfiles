843d3aa9a38c81f25f7c890daa1ea3f7e0a0840b
Simon Ward <39803787+simonward87@users.noreply.github.com>
2025-05-17 17:51:10 +0100

    Update submodule references

--- config/zsh/funcs.zsh
+++ config/zsh/funcs.zsh
@@ -10,13 +10,13 @@ function theme () {
     if defaults read -g AppleInterfaceStyle &>/dev/null; then
         if grep -E "da(y|wn).theme" $DOTFILES/tmux.conf; then
             sed -i -E 's/da(y|wn).theme/dusk.theme/' $DOTFILES/tmux.conf
-            sed -i -E 's/k-da(y|wn)/k-dusk/' $NVIM_CONF/lua/$USER/colorscheme.lua
+            sed -i -E 's/k-da(y|wn)/k-dusk/' $NVIM_CONF/lua/$USER/init.lua
             sed -i -E 's/k\/da(y|wn)/k\/dusk/' $DOTFILES/config/alacritty/alacritty.toml
         fi
     else
         if grep -E "d(us|ar)k.theme" $DOTFILES/tmux.conf; then
             sed -i -E 's/d(ar|us)k.theme/dawn.theme/' $DOTFILES/tmux.conf
-            sed -i -E 's/k-d(ar|us)k/k-dawn/' $NVIM_CONF/lua/$USER/colorscheme.lua
+            sed -i -E 's/k-d(ar|us)k/k-dawn/' $NVIM_CONF/lua/$USER/init.lua
             sed -i -E 's/k\/d(ar|us)k/k\/dawn/' $DOTFILES/config/alacritty/alacritty.toml
         fi
     fi
